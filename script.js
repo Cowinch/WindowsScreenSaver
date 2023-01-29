a8360d2f451dc94938977d4a202f469f147aa49c
Cole Winchester <cowinch@gmail.com>
2023-01-28 23:17:42 -0600

    added 3d cube capability

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.getElementById('bouncy').style.animationPlayState='paused'
 //Bouncy box paramteres
 let x=0,
     y=0,
@@ -10,9 +11,15 @@ let running=false
 function bounce(){
     if(running){
         //Collision parameters (these are placed in the function so they have a chance to update as the page is moved)
-        let containerWidth=document.getElementById('container').offsetWidth,
-            containerHeight=document.getElementById('container').offsetHeight,
+        let containerMaxWidth=document.getElementById('container').offsetWidth,
+            containerMaxHeight=document.getElementById('container').offsetHeight,
+            containerMin=0,
             boxSize=document.getElementById('bouncy').offsetWidth
+
+        if(dimensions){
+            boxSize=document.getElementById('bouncy').offsetWidth+(boxSize*.1)
+            containerMin=boxSize*.15
+        }
         
         //checklist for determining the directions to move in
         if(xMovement=='left'){
@@ -27,17 +34,17 @@ function bounce(){
         }
 
         //collision check for when the box bumps into the egde of the container. changes direction.
-        if(x>=containerWidth-boxSize){
-            x=containerWidth-boxSize
+        if(x>=containerMaxWidth-boxSize){
+            x=containerMaxWidth-boxSize
             xMovement='right'
         }
-        if(x<=0) xMovement='left'
+        if(x<=containerMin) xMovement='left'
 
-        if(y>=containerHeight-boxSize){
-            y=containerHeight-boxSize
+        if(y>=containerMaxHeight-boxSize){
+            y=containerMaxHeight-boxSize
             yMovement='up'
         }
-        if(y<=0) yMovement='down'
+        if(y<=containerMin) yMovement='down'
 
         //updates position of the box
         document.documentElement.style.setProperty('--x-axis', `${x}px`)
@@ -45,16 +52,33 @@ function bounce(){
     }
 }
 
+//logic for 3d box
+let dimensions=false
+function threeDimensions(){
+    if(dimensions){
+        dimensions=false
+        document.documentElement.style.setProperty('--transform-style', `none`)
+        document.documentElement.style.setProperty('--animation', `none`)
+
+    } else if (!dimensions){
+        dimensions=true
+        document.documentElement.style.setProperty('--transform-style', `preserve-3d`)
+        document.documentElement.style.setProperty('--animation', `animateSpin 10s linear infinite`)
+    }
+}
+
 //logic for the button
 function animation(){
     if(!running){
         running=true
+        document.getElementById('bouncy').style.animationPlayState='running'
         document.getElementById('the-button').innerText='Stop'
     }
     else if(running){
         running=false
+        document.getElementById('bouncy').style.animationPlayState='paused'
         document.getElementById('the-button').innerText='Start'
         
     }
 }
-setInterval(bounce, 15)
+setInterval(bounce, 10)
